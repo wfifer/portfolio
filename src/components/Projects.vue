b00d745a84a541f7d6cc53705182922b44d34c78
Will Fifer <will@thirststudios.com>
2018-01-03 15:15:09 +1100

    Vue conversion complete

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,7 +1,7 @@
 <template>
 	<section class="site-header" @mousemove="onMouseMove">
 		<div class="inner">
-			<div v-for="(project, index) in projects" class="project-mask" :class="{ '-active': index == activeProject }" :key="index">
+			<div v-for="(project, index) in projects" class="project-mask" :class="projectClass[index]" :key="index">
 				<div class="mask-inner">
 					<div class="mask-svg">
 						<svg x="0px" y="0px" viewBox="0 0 800 800" style="enable-background:new 0 0 800 800;" xml:space="preserve">
@@ -22,8 +22,10 @@
 
 								<rect x="400" y="400" width="1500" height="1500" :style="gradientTransform[index]" />
 
-								<g class="project-hero" :style="backgroundPosition">
-									<image width="800" height="800" style="overflow:visible;" :xlink:href="project.heroImage.url" x="400" y="400"></image>
+								<g>
+									<g class="project-hero" :style="backgroundPosition">
+										<image width="800" height="800" style="overflow:visible;" :xlink:href="project.heroImage.url" x="400" y="400"></image>
+									</g>
 								</g>
 
 								<rect x="400" y="400" width="1500" height="1500" :style="'mix-blend-mode: hue; opacity: 0.5; ' + gradientTransform[index]" />
@@ -53,11 +55,18 @@
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import TWEEN from '@tweenjs/tween.js';
 
 export default {
 	name: 'Projects',
 	mounted () {
 		this.getProjects();
+	},
+	data: function () {
+		return {
+			projectClass: [ '-active' ],
+			bgCoords: { x: 0, y: 0 }
+		};
 	},
 	methods: {
 		enterAbout () {
@@ -80,11 +89,59 @@ export default {
 			'updateProjectMouse'
 		])
 	},
+	watch: {
+		activeProject: function (newValue, oldValue) {
+			this.projectClass = this.projects.map((project, index) => {
+				let pClass = [];
+
+				if (this.activeProject === index) {
+					pClass.push('-animate-in');
+				} else if (this.lastProject === index) {
+					pClass.push('-active', '-animate-out');
+
+					pClass.push(this.direction === 1 ? '-left' : '-right');
+				}
+				return pClass;
+			});
+
+			setTimeout(() => {
+				this.projectClass = this.projects.map((project, index) => {
+					let pClass = this.activeProject === index ? ['-active'] : [];
+					return pClass;
+				});
+			}, 2000);
+		},
+		mouse: function (newMouse, oldMouse) {
+			var vm = this;
+
+			function animate () {
+				if (TWEEN.update()) {
+					requestAnimationFrame(animate);
+				}
+			};
+
+			const x = (vm.window.width / 2 - newMouse.x) / vm.window.width * 30;
+			const y = (vm.window.width / 2 - newMouse.y) / vm.window.width * 30;
+
+			new TWEEN.Tween(vm.bgCoords)
+				.easing(TWEEN.Easing.Quintic.Out)
+				.to({ x: x, y: y }, 650)
+				.onUpdate(function () {
+					console.log(vm.bgCoords);
+				})
+				.start();
+
+			animate();
+		}
+	},
 	computed: {
 		gradientTransform () {
 			return this.projects.map(function (project, index) {
 				return `fill: url(#gradient-bg-${ index }); transform: ${ project.heroBackground.angle.length ? 'rotate(' + project.heroBackground.angle + ')' : '' } translate(-50%, -50%)`;
 			});
+		},
+		backgroundPosition () {
+			return `transform: translate3d(${ this.bgCoords.x }px, ${ this.bgCoords.y }px, 0)`;
 		},
 		projectInitial () {
 			return this.projects.map(function (project) {
@@ -96,14 +153,10 @@ export default {
 				return project.title.replace(/(\S)/g, '<span>$1</span>').replace(/(\S+)/g, '<div>$1</div>');
 			});
 		},
-		backgroundPosition () {
-			const x = (this.window.width / 2 - this.mouse.x) / this.window.width * 50;
-			const y = (this.window.width / 2 - this.mouse.y) / this.window.width * 50;
-
-			return `transform: translate3d(${ x }px, ${ y }px, 0)`;
-		},
 		...mapState({
 			activeProject: state => state.projects.active,
+			lastProject: state => state.projects.last,
+			direction: state => state.projects.direction,
 			projects: state => state.projects.projects,
 			mouse: state => state.projects.mouse,
 			window: state => state.app.window
--- src/store/modules/projects/index.js
+++ src/store/modules/projects/index.js
@@ -5,14 +5,17 @@ import actions from './actions';
 
 const state = {
 	active: 0,
+	last: 0,
+	direction: 0,
 	projects: [],
 	mouse: {}
 };
 
 const mutations = {
 	[type.NAVIGATE_PROJECTS] (state, action) {
+		state.last = state.active;
 		state.active = (state.active + action.direction + state.projects.length) % state.projects.length;
-		console.log(state.projects.length, state.active);
+		state.direction = action.direction;
 	},
 	[type.GET_PROJECTS] (state, action) {
 		state.projects = action.response.data.data;
